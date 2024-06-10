104ed376a653a56d06b81b9cdfc441795802b39c
Luis María García Salado <lgarciasaladokuhl@outlook.com>
2024-06-10 09:32:22 -0300

    Fix region

--- GSP073.sh
+++ GSP073.sh
@@ -1,7 +1,6 @@
 
 read -p 'Ingress location' LOCATION
-read -p 'Ingress region: ' REGION
 
 PROJECT_ID=$(gcloud config get-value project)
 
-gcloud storage buckets create gs://$PROJECT_ID --default-storage-class=standard --location=$LOCATION --placement=$REGION --uniform-bucket-level-access --no-public-access-prevention
+gcloud storage buckets create gs://$PROJECT_ID --default-storage-class=standard --location=$LOCATION  --uniform-bucket-level-access --no-public-access-prevention
