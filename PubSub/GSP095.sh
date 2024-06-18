3acc051c791ee0be08a699069970b20e559608a9
Luis María García Salado <lgarciasaladokuhl@outlook.com>
2024-06-18 13:08:38 -0300

    (PUB/SUB) Done script lab

---
+++ PubSub/GSP095.sh
@@ -0,0 +1,75 @@
+echo 'Lab GSP095'
+
+echo '####################################################'
+echo 'Task 1. Pub/Sub topics'
+echo '####################################################'
+    
+gcloud pubsub topics create myTopic
+
+input 'Validate task'
+
+echo 'Creating two more topics'
+gcloud pubsub topics create Test1
+gcloud pubsub topics create Test2
+
+echo 'Listing the tree topics'
+gcloud pubsub topics list
+
+echo '#################################'
+echo 'Time to clean up. Delete Test1 and Test2'
+
+gcloud pubsub topics delete Test1
+gcloud pubsub topics delete Test2
+
+echo 'time to verify the topics were deleted'
+gcloud pubsub topics list
+
+echo '####################################################'
+echo 'Task 2. Pub/Sub subscriptions'
+echo '####################################################'
+
+echo 'create a subscription called mySubscription to topic myTopic'
+gcloud  pubsub subscriptions create --topic myTopic mySubscription
+
+input 'Validate task'
+
+echo 'Add another two subscriptions to myTopic'
+gcloud  pubsub subscriptions create --topic myTopic Test1
+gcloud  pubsub subscriptions create --topic myTopic Test2
+
+echo 'list the subscriptions to myTopic: '
+gcloud pubsub topics list-subscriptions myTopic
+
+echo '#################################'
+echo 'Time to clean up. Delete Test1 and Test2'
+
+gcloud pubsub subscriptions delete Test1
+gcloud pubsub subscriptions delete Test2
+
+echo '####################################################'
+echo 'Task 3. Pub/Sub publishing and pulling a single message'
+echo '####################################################'
+
+echo 'publish the message "hello" to the topic you created previously (myTopic)'
+gcloud pubsub topics publish myTopic --message "Hello"
+gcloud pubsub topics publish myTopic --message "Publisher's name is Luis"
+gcloud pubsub topics publish myTopic --message "Publisher likes to eat Asado"
+gcloud pubsub topics publish myTopic --message "Publisher thinks Pub/Sub is awesome"
+
+echo 'pull the messages you just published from the Pub/Sub topic: '
+gcloud pubsub subscriptions pull mySubscription --auto-ack
+gcloud pubsub subscriptions pull mySubscription --auto-ack
+gcloud pubsub subscriptions pull mySubscription --auto-ack
+gcloud pubsub subscriptions pull mySubscription --auto-ack
+
+input 'Validate task'
+echo '####################################################'
+echo 'Task 4. Pub/Sub pulling all messages from subscriptions'
+echo '####################################################'
+echo 'publish the message "hello" to the topic you created previously (myTopic)'
+gcloud pubsub topics publish myTopic --message "Hello"
+gcloud pubsub topics publish myTopic --message "Publisher's name is Luis"
+gcloud pubsub topics publish myTopic --message "Publisher likes to eat Asado"
+gcloud pubsub topics publish myTopic --message "Publisher thinks Pub/Sub is awesome"
+echo 'Run the pull command with the limit flag'
+gcloud pubsub subscriptions pull mySubscription --auto-ack --limit=3
--- PubSub/README.md
+++ PubSub/README.md
@@ -11,5 +11,16 @@ Pub/Sub is used for streaming analytics and data integration pipelines to load a
 Pub/Sub lets you create systems of event producers and consumers, called publishers and subscribers. Publishers communicate with subscribers asynchronously by broadcasting events, rather than by synchronous remote procedure calls (RPCs).
 
 Publishers send events to the Pub/Sub service, without regard to how or when these events are to be processed. Pub/Sub then delivers events to all the services that react to them. In systems communicating through RPCs, publishers must wait for subscribers to receive the data. However, the asynchronous integration in Pub/Sub increases the flexibility and robustness of the overall system.
+
+## Pub/Sub basics
+As stated earlier, Pub/Sub is an asynchronous global messaging service. There are three terms in Pub/Sub that appear often: topics, publishing, and subscribing.
+
+- A topic is a shared string that allows applications to connect with one another through a common thread.
+
+- Publishers push (or publish) a message to a Cloud Pub/Sub topic.
+
+- Subscribers make a "subscription" to a topic where they will either pull messages from the subscription or configure webhooks for push subscriptions. Every subscriber must acknowledge each message within a configurable window of time.
+
+To sum it up, a producer publishes messages to a topic and a consumer creates a subscription to a topic to receive messages from it.
 ## Use Cases
 [Documentación](https://cloud.google.com/pubsub/docs/overview#common_use_cases)
